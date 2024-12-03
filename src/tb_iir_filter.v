acb4a9b41ffcff17ee91d704c9c8c279291b0ae4
PhamNghia <88358944+Jerguel02@users.noreply.github.com>
2024-12-03 16:34:16 +0700

    debug: cannot read coeff

--- src/tb_iir_filter.v
+++ src/tb_iir_filter.v
@@ -2,8 +2,8 @@ module tb_iir_filter;
 
     reg clk;
     reg reset;
-    reg signed [15:0] x;       // Tín hiệu đầu vào
-    wire signed [15:0] y;      // Tín hiệu đầu ra
+    reg signed [15:0] x;    
+    wire signed [15:0] y;      
 
     // Clock 10ns (100MHz)
     initial clk = 0;
@@ -17,18 +17,18 @@ module tb_iir_filter;
     );
 
     initial begin
-        // Reset hệ thống
+
         reset = 1;
         #20;
         reset = 0;
 
-        // Cấp tín hiệu đầu vào
+
         x = 16'd0;    #10;
         x = 16'd100;  #10;
-        x = 16'd50;   #10;
-        x = 16'd100; #10;
+        x = 16'd150;   #10;
+        x = 16'd200; #10;
 
-        // Kết thúc mô phỏng
+
         #100 $stop;
     end
 endmodule
